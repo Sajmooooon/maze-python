400ddac2490307b8872c72a8da3fd05e305ded17
simon <sajmoooonsk@gmail.com>
2022-09-15 12:16:58 +0200

    feat: add player

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
 
+class Player:
+    def __init__(self, block_size):
+        self.block_size = block_size
+        self.x = 50
+        self.y = 300
 
 class App:
 
@@ -7,12 +12,21 @@ class App:
         self.screen = None
         self.run = True
         self.speed = 50
+        self.block_size = 50
         self.clock = pygame.time.Clock()
+        self.player = Player(self.block_size)
+        self.player_img = None
 
     def on_init(self):
         pygame.init()
         self.screen = pygame.display.set_mode((400, 400))
         pygame.display.set_caption("Maze")
+
+        self.player_img = pygame.Surface([50, 50])
+        self.player_img.fill((0, 0, 255))
+
+    def draw_player(self):
+        self.screen.blit(self.player_img, (self.player.x, self.player.y))
 
     def start_game(self):
         self.on_init()
@@ -23,6 +37,7 @@ class App:
                 if event.type == pygame.QUIT:
                     self.run = False
 
+            self.draw_player()
             pygame.display.update()
             self.clock.tick(self.speed)
 
